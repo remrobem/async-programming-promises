2487c66090c0fd31a84bb3349b1cb67607c8b40d
Rob Martin <rob.martin.5758@gmail.com>
2020-01-14 15:16:39 -0500

    async/await iterating

--- src/iterating.mjs
+++ src/iterating.mjs
@@ -1,18 +1,68 @@
-import setText , {appendText} from './results.mjs';
+import setText, { appendText } from './results.mjs';
 
-export function get(){
+export async function get() {
+    // use await on function that returns promise but async must still be specified
+    const { data } = await axios.get('http://localhost:3000/orders/1');
+    setText(JSON.stringify(data));
 }
 
-export function getCatch(){
+export async function getCatch() {
+    // try/catch block is not just for async/await. Std JS stuff
+    try {
+        const { data } = await axios.get('http://localhost:3000/orders/123');
+        setText(JSON.stringify(data));
+    } catch (error) {
+        setText(error)
+    }
 }
 
-export function chain(){
+export async function chain() {
+    setText("");
+
+    appendText(" waiting on orders ");
+    const { data } = await axios.get('http://localhost:3000/orders/1');
+    appendText(JSON.stringify(data));
+
+    console.log(`Data: ${JSON.stringify(data)}`);
+
+    appendText(" waiting on status ");
+    const { data: address } = await axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+    console.log(`Address: ${JSON.stringify(address)}`);
+    appendText(`City: ${address.city}`)
+
 }
 
-export function concurrent(){
+export async function concurrent() {
+    // 2 axios calls started, then await for each to finish
+    const orderStatus = axios.get('http://localhost:3000/orderStatuses');
+    const orders = axios.get('http://localhost:3000/orders');
+
+    setText("");
+    appendText(" waiting on orders ");
+    const { data: order } = await orders;
+    appendText(" waiting on status ");
+    const { data: statuses } = await orderStatus;
+
+
+    appendText(JSON.stringify(statuses))
+    appendText(JSON.stringify(order[0]))
 }
 
-export function parallel(){
+export async function parallel() {
+    setText("");
+    // Promise all with anonymouse function IIFE's for each item in promise array
+    await Promise.all([
+        (async () => {
+            appendText(" waiting on status ");
+            const { data: statuses } = await axios.get('http://localhost:3000/orderStatuses');
+            appendText(` Status: ${JSON.stringify(statuses)}`)
+        })(),
+        (async () => {
+            appendText(" waiting on orders ");
+            const { data: orders } = await axios.get('http://localhost:3000/orders');
+            appendText(` Orders: ${JSON.stringify(orders)}`)
+        })(),
+    ])
 }
 
 
